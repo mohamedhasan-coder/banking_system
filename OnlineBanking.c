be15b4a9a574e6936a0677db2b763d512c13af45
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-26 12:22:15 +0530

    Update CLEAR_SCREEN command for Windows

--- OnlineBanking.c
+++ OnlineBanking.c
@@ -4,7 +4,7 @@
 
 // Define a command to clear the screen that works on both Windows and Linux/macOS
 #ifdef _WIN32
-#define CLEAR_SCREEN ""
+#define CLEAR_SCREEN "cls"
 #else
 #define CLEAR_SCREEN "clear"
 #endif
